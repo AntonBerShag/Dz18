2bf3400bd1154e704c9fd2b48dc9909594e389f7
Berd_ur18@itstepru.onmicrosoft.com <Berd_ur18@itstepru.onmicrosoft.com>
2022-02-17 15:29:12 +0500

    Поправил код

--- Dz18/Dz18.cpp
+++ Dz18/Dz18.cpp
@@ -46,25 +46,25 @@ int max(int num1, int num2, int num3) {
 	if (num1 >= num2 && num1 >= num3)
 		return num1;
 	else if (num2 > num1 && num2 >= num3)
-		return num2;
-	else
-		return num3;
+			return num2;
+		else
+			return num3;
 }
 float max(float num1, float num2, float num3) {
-	if (num1 >= num2 && num1 >= num3)
-		return num1;
-	else if (num2 > num1 && num2 >= num3)
-		return num2;
-	else
-		return num3;
-}
-double max(double num1, double num2, double num3) {
 	if (num1 >= num2 && num1 >= num3)
 		return num1;
 	else if (num2 > num1 && num2 >= num3)
 			return num2;
 		else
 			return num3;
+}
+double max(double num1, double num2, double num3) {
+	if (num1 >= num2 && num1 >= num3)
+		return num1;
+	else if (num2 > num1 && num2 >= num3)
+				return num2;
+			else
+				return num3;
 }
 //Задача 1
 inline double srAr(double num1, double num2) {
